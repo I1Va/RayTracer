bb55e15022560b7426b10b268416f2f2453f72a1
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-10-10 01:53:13 +0300

    redesign architecture

--- inc/Camera.h
+++ inc/Camera.h
@@ -1,7 +1,17 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+
+#include <vector>
 #include "Geom.h"
+
+
+struct RTColor {
+    int r = 0;
+    int g = 0;
+    int b = 0;
+    int a = 0;
+};
 
 class Ray {
 
@@ -32,13 +42,17 @@ struct Viewport {
 class Camera {
     static constexpr const double FOCAL_LENGTH = 1;
 
-    GmPoint<double, 3> center_;
-    GmVec<double, 3> direction_;
-    Viewport viewPort_;
+    GmPoint<double, 3> center_ = {};
+    GmVec<double, 3> direction_ = {};
+    Viewport viewPort_ = {};
 
+    std::pair<int, int> screenResolution_ = {};
+    std::vector<RTColor> pixels_ = {};
 public:
-    Camera(const GmPoint<double, 3> &center, const GmVec<double, 3> &direction): 
-        center_(center), direction_(direction.normalized()) 
+    Camera(): center_{}, direction_(0, 0, 1) {}
+ 
+    Camera(const GmPoint<double, 3> &center, const GmVec<double, 3> &direction, const std::pair<int, int> &screenResolution): 
+        center_(center), direction_(direction.normalized()), screenResolution_(screenResolution), pixels_(screenResolution.first * screenResolution.second)
     {
         GmVec<double,3> A(1,0,0);
         if (std::abs(direction_.x()) > 0.999) A = GmVec<double,3>(0,1,0);
@@ -48,10 +62,10 @@ public:
         viewPort_.upperLeft_ = center_ + direction_ * FOCAL_LENGTH - viewPort_.rightDir_ * 0.5 - viewPort_.downDir_ * 0.5; 
     };
 
-    Ray genRay(int pixelX, int pixelY, std::pair<int, int> resolution) {
+    Ray genRay(int pixelX, int pixelY) {
 
-        double deltaWidth = viewPort_.VIEWPORT_WIDTH / resolution.first;
-        double deltaHeight = viewPort_.VIEWPORT_HEIGHT / resolution.second;
+        double deltaWidth = viewPort_.VIEWPORT_WIDTH / screenResolution_.first;
+        double deltaHeight = viewPort_.VIEWPORT_HEIGHT / screenResolution_.second;
 
         GmPoint<double, 3> viewPortPoint = viewPort_.upperLeft_                              +
                                            viewPort_.rightDir_ * (pixelX + 0.5) * deltaWidth +
@@ -61,6 +75,8 @@ public:
 
         return Ray(center_, rayDirection);
     }
+
+    const std::pair<int, int> &screenResolution() const { return screenResolution_; }
 };
 
 
--- inc/RayTracer.h
+++ inc/RayTracer.h
@@ -1,8 +1,40 @@
 #ifndef RAY_TRACER_H
 #define RAY_TRACER_H
 
+#include "Geom.h"
+
+class SceneObject {
+    GmVec<double, 3> pos_;
+};
+
 
 class SceneManager {
+    std::vector<SceneObject *> sceneObjects_;
+
+public:
+    explicit SceneManager() {};
+
+    
+    void render(Camera &camera) {
+        update();
+
+        computeCameraPixelBufer(camera);
+    }
+
+private:
+    void update() {}
+
+    void computeCameraPixelBufer(Camera &camera) {
+        for (int pixelX = 0; pixelX < camera.screenResolution().first; pixelX++) {
+            for (int pixelY = 0; pixelY < camera.screenResolution().second; pixelY++) {
+                Ray ray = camera.genRay(pixelX, pixelY);
+
+                
+
+            }
+        }
+    }
+    
 
 };
 
